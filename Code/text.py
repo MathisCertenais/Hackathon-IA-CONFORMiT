68a32f6d866f58444d1dbcd065c3d2661a73745a
Mathis Certenais <mathiscertenais@gmail.com>
2022-11-05 17:09:06 -0400

    Add P&ID png

--- Code/text.py
+++ Code/text.py
@@ -1,12 +1,15 @@
 import pytesseract
 import cv2
-img = cv2.imread('test2.png')
+img = cv2.imread('../P&ID/png/SIMPLE-2-1.png')
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = cv2.resize(img, (600, 360))
-# Convertir l'image en String
-print(pytesseract.image_to_string(img))
-# Récupérer la position de chaque lettre des textes de l'image
-print(pytesseract.image_to_boxes(img))
-cv2.imshow('Result', img)
-cv2.waitKey(0)
+if img is None:
+    print('Wrong path')
+else:
+    #img = cv2.resize(img, (600, 360))
+    # Convertir l'image en String
+    print(pytesseract.image_to_string(img))
+    # Récupérer la position de chaque lettre des textes de l'image
+    print(pytesseract.image_to_boxes(img))
+    cv2.imshow('Result', img)
+    cv2.waitKey(0)
